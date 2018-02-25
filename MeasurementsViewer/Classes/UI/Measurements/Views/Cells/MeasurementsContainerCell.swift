0f30163bfad3978dd34970ee4d10d986416eb757
Vladislav Solovyov <vlad@moqod.com>
2018-02-25 22:53:21 +0400

    Tests added, little code refactoring.

--- MeasurementsViewer/Classes/UI/Measurements/Views/Cells/MeasurementsContainerCell.swift
+++ MeasurementsViewer/Classes/UI/Measurements/Views/Cells/MeasurementsContainerCell.swift
@@ -38,6 +38,7 @@ class MeasurementsContainerCell: BaseTableViewCell {
             } else {
                 self.measurmentsCountLabel.text = ""
             }
+            
             self.setNeedsLayout()
         }
     }
